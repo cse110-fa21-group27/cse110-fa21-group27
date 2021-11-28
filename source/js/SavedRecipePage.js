2e7e6739550a469ea8f77bfb54d9a66b1a0298f3
Tucker Frandsen <tfrandsen@ucsd.edu>
2021-11-27 19:37:35 -0800

    Created a basic SavedRecipePage.js. Added it to index.html.

---
+++ source/js/SavedRecipePage.js
@@ -0,0 +1,44 @@
+/** Class that creates a Saved Recipe Page HTML Component */
+class SavedRecipePage extends HTMLElement {
+  /** Constructs the Component and allows access to the shadow */
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+  }
+
+  /**
+   * Populates the Saved Recipe Page HTML Component with information from the
+   * recipe json file and displays it with some CSS styling.
+   * @param {Object} data - Unknown at this point
+   */
+  set data(data) {
+    const style = `
+    button {
+      height: 5vh;
+      width: 10vw;
+    }`;
+
+    const styleElem = document.createElement("style");
+    styleElem.innerHTML = style;
+
+    const page = document.createElement("article");
+    const form = document.createElement("form");
+    const input = document.createElement("input");
+    input.textContent = "Collection Name";
+    form.appendChild(input);
+
+    const newCollection = document.createElement("button");
+    newCollection.textContent = "Create new Collection";
+    newCollection.addEventListener("click", () => {
+      if (input.textContent != "Collection Name" && input.textContent != "") {
+        // Create new Collection
+        // Somehow add saved recipes to it
+      }
+    });
+
+    this.shadowRoot.appendChild(styleElem);
+    this.shadowRoot.appendChild(page);
+  }
+}
+
+customElements.define("saved-recipe-page", SavedRecipePage);
