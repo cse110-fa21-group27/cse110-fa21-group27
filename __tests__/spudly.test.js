93a523292f0d65b58d40981aa509ede9ea1d0fde
ArturoAmaya <a1amaya@ucsd.edu>
2021-11-30 12:16:45 -0800

    wrote a test to check population of the recipe card elements

--- __tests__/spudly.test.js
+++ __tests__/spudly.test.js
@@ -20,6 +20,7 @@ describe("Basic user flow for Website", () => {
     await delay(4000);
     console.log("Checking for 264 product items...");
     // Query select all of the <recipe-card> elements and return the length of that array
+    await page.screenshot({ path: "scrrenshot57.png" });
     const numCards = await page.$$eval("recipe-card", (prodItems) => {
       return prodItems.length;
     });
@@ -27,46 +28,56 @@ describe("Basic user flow for Website", () => {
     expect(numCards).toBe(264);
   });
 
-  // Check to make sure that all 20 <product-item> elements have data in them
-  it("Make sure <product-item> elements are populated", async () => {
+  // Check to make sure that all 264 <recipe-card> elements have "good" data in them
+  it("Make sure <recipe-card> elements are populated", async () => {
     console.log(
-      "Checking to make sure <product-item> elements are populated..."
+      "Checking to make sure <recipe-card> elements are populated..."
     );
+    await delay(4000);
     // Start as true, if any don't have data, swap to false
     let allArePopulated = true;
-    let data, plainValue;
-    // Query select all of the <product-item> elements
-    const prodItems = await page.$$("product-item");
+    let plainValue, img, imgSrc, p, innerText;
+    // Query select all of the <recipe-card> elements
+    const prodItems = await page.$$("recipe-card");
+    //await page.screenshot({ path: "scrrenshot57.png" });
+
     for (let i = 0; i < prodItems.length; i++) {
-      console.log(`Checking product item ${i + 1}/${prodItems.length}`);
-      // Grab the .data property of <product-items> to grab all of the json data stored inside
-      data = await prodItems[i].getProperty("data");
-      // Convert that property to JSON
-      plainValue = await data.jsonValue();
-      // Make sure the title, price, and image are populated in the JSON
-      if (plainValue.title.length == 0) {
+      // console.log(`Checking recipe card ${i + 1}/${prodItems.length}`);
+
+      // Grab the shadowRoot and pull the img, and p tags
+      const itemShadow = await prodItems[i].getProperty("shadowRoot");
+      img = await itemShadow.$("img");
+      imgSrc = await img.getProperty("src");
+      plainValue = await imgSrc.jsonValue();
+
+      p = await itemShadow.$("p");
+      innerText = await p.getProperty("innerText");
+      innerText = await innerText.jsonValue();
+      // console.log(plainValue);
+      if (
+        plainValue == "" ||
+        plainValue == "https://spudly-f0411.web.app/undefined" ||
+        innerText == ""
+      ) {
         allArePopulated = false;
-      }
-      if (plainValue.price.length == 0) {
-        allArePopulated = false;
-      }
-      if (plainValue.image.length == 0) {
-        allArePopulated = false;
+        console.log(
+          `Recipe card #${i + 1} either doesn't have an image or a description`
+        );
       }
     }
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
 
     // TODO - Step 1
-    // Right now this function is only checking the first <product-item> it found, make it so that
-    // it checks every <product-item> it found
-  }, 10000);
-
-  // Check to make sure that when you click "Add to Cart" on the first <product-item> that
+    // Right now this function is only checking the first <recipe-card> it found, make it so that
+    // it checks every <recipe-card> it found
+  }, 20000);
+  /*
+  // Check to make sure that when you click "Add to Cart" on the first <recipe-card> that
   // the button swaps to "Remove from Cart"
   it('Clicking the "Add to Cart" button should change button text', async () => {
     console.log('Checking the "Add to Cart" button...');
-    const prodItem = await page.$("product-item");
+    const prodItem = await page.$("recipe-card");
     const itemShadow = await prodItem.getProperty("shadowRoot");
     let shadowButton = await itemShadow.$("button");
     await shadowButton.click();
@@ -74,21 +85,21 @@ describe("Basic user flow for Website", () => {
     const newbuttontext = innerText["_remoteObject"].value;
     expect(newbuttontext).toBe("Remove from Cart");
     // TODO - Step 2
-    // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    // Query a <recipe-card> element using puppeteer ( checkout page.$() and page.$$() in the docs )
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
     // Once you have the button, you can click it and check the innerText property of the button.
     // Once you have the innerText property, use innerText['_remoteObject'].value to get the text value of it
   }, 2500);
 
-  // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
+  // Check to make sure that after clicking "Add to Cart" on every <recipe-card> that the Cart
   // number in the top right has been correctly updated
   it("Checking number of items in cart on screen", async () => {
     console.log("Checking number of items in cart on screen...");
     // TODO - Step 3
-    // Query select all of the <product-item> elements, then for every single product element
+    // Query select all of the <recipe-card> elements, then for every single product element
     // get the shadowRoot and query select the button inside, and click on it.
     // use $$ to select all
-    const prodItems = await page.$$("product-item");
+    const prodItems = await page.$$("recipe-card");
     // start at 1 because we already clicked the first one
     for (let i = 1; i < prodItems.length; i++) {
       const itemShadow = await prodItems[i].getProperty("shadowRoot");
@@ -110,11 +121,11 @@ describe("Basic user flow for Website", () => {
   it("Checking number of items in cart on screen after reload", async () => {
     console.log("Checking number of items in cart on screen after reload...");
     // TODO - Step 4
-    // Reload the page, then select all of the <product-item> elements, and check every
+    // Reload the page, then select all of the <recipe-card> elements, and check every
     // element to make sure that all of their buttons say "Remove from Cart".
     // Also check to make sure that #cart-count is still 20
     await page.reload();
-    const prodItems = await page.$$("product-item");
+    const prodItems = await page.$$("recipe-card");
     for (let i = 0; i < prodItems.length; i++) {
       const itemShadow = await prodItems[i].getProperty("shadowRoot");
       let shadowButton = await itemShadow.$("button");
@@ -148,9 +159,9 @@ describe("Basic user flow for Website", () => {
   it("Checking number of items in cart on screen after removing from cart", async () => {
     console.log("Checking number of items in cart on screen...");
     // TODO - Step 6
-    // Go through and click "Remove from Cart" on every single <product-item>, just like above.
+    // Go through and click "Remove from Cart" on every single <recipe-card>, just like above.
     // Once you have, check to make sure that #cart-count is now 0
-    const prodItems = await page.$$("product-item");
+    const prodItems = await page.$$("recipe-card");
     for (let i = 0; i < prodItems.length; i++) {
       const itemShadow = await prodItems[i].getProperty("shadowRoot");
       let shadowButton = await itemShadow.$("button");
@@ -169,11 +180,11 @@ describe("Basic user flow for Website", () => {
   it("Checking number of items in cart on screen after reload", async () => {
     console.log("Checking number of items in cart on screen after reload...");
     // TODO - Step 7
-    // Reload the page once more, then go through each <product-item> to make sure that it has remembered nothing
+    // Reload the page once more, then go through each <recipe-card> to make sure that it has remembered nothing
     // is in the cart - do this by checking the text on the buttons so that they should say "Add to Cart".
     // Also check to make sure that #cart-count is still 0
     await page.reload();
-    const prodItems = await page.$$("product-item");
+    const prodItems = await page.$$("recipe-card");
     for (let i = 0; i < prodItems.length; i++) {
       const itemShadow = await prodItems[i].getProperty("shadowRoot");
       let shadowButton = await itemShadow.$("button");
@@ -200,5 +211,5 @@ describe("Basic user flow for Website", () => {
     const cart = localStorage["cart"];
     const expectedCart = "[]";
     expect(cart).toBe(expectedCart);
-  });
+  });*/
 });
