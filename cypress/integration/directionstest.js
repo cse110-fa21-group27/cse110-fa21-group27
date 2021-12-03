2622d1e6cb3b81bca4b34c5a920b78f9c7d45616
ArturoAmaya <a1amaya@ucsd.edu>
2021-12-03 13:36:10 -0800

    some more directions unit tests

--- cypress/integration/directionstest.js
+++ cypress/integration/directionstest.js
@@ -6,60 +6,91 @@ console.log(recipe);
 const numSteps = recipe.recipe1.analyzedInstructions[0].steps.length;
 
 // https://on.cypress.io/writing-first-test
-describe("Open Page", { timeout: 10000 }, () => {
-  it("Opens index.html", () => {
-    cy.visit("./__tests__/unit_html/directions_test.html");
-  });
+describe(
+  "UNIT TEST DIRECTIONS: essentially, given good info does it do what we want?",
+  { timeout: 10000 },
+  () => {
+    it("Opens index.html", () => {
+      cy.visit("./__tests__/unit_html/directions_test.html");
+    });
 
-  // Check how many direction pages there are
-  it("check how many direction pages there are", () => {
-    cy.get("directions-info").should("have.length", 1);
-  });
+    // Check how many direction pages there are
+    it("check how many direction pages there are", () => {
+      cy.get("directions-info").should("have.length", 1);
+    });
 
-  // Check that the title says "Directions"
-  it('check that the title says "Directions"', () => {
-    cy.get("directions-info")
-      .shadow()
-      .find("p")
-      .then(($el) => {
-        expect($el[0].innerHTML).to.be.equal("Directions");
-      });
-  });
-  // Check how many directions there are. Should be greater than 0.
-  it("check how many steps there are", () => {
-    cy.get("directions-info")
-      .shadow()
-      .find(".direction")
-      .then(($el) => {
-        expect($el.length).to.be.greaterThan(0);
-        expect($el.length).to.be.equal(numSteps);
-      });
-  });
-
-  // Check all the directions steps
-  for (let i = 0; i < numSteps; i++) {
-    it(`check that instruction ${i} is non-empty`, () => {
+    // Check that the title says "Directions"
+    it('check that the title says "Directions"', () => {
       cy.get("directions-info")
         .shadow()
-        .find(".text")
+        .find("p")
         .then(($el) => {
-          expect($el).to.not.be.equal("");
+          expect($el[0].innerHTML).to.be.equal("Directions");
+        });
+    });
+    // Check how many directions there are. Should be greater than 0.
+    it("check how many steps there are", () => {
+      cy.get("directions-info")
+        .shadow()
+        .find(".direction")
+        .then(($el) => {
+          expect($el.length).to.be.greaterThan(0);
+          expect($el.length).to.be.equal(numSteps);
         });
     });
 
-    it(`check that instruction ${i} is correctly parsed`, () => {
+    // check how many checkboxes there are
+    it("check how many checkboxes there are", () => {
       cy.get("directions-info")
         .shadow()
-        .find(".text")
+        .find("input")
         .then(($el) => {
-          expect($el[i].innerHTML).to.be.equal(
-            `${i + 1}) ` + recipe.recipe1.analyzedInstructions[0].steps[i].step
-          );
+          expect($el.length).to.be.equal(numSteps);
         });
     });
+
+    // Check all the directions steps
+    for (let i = 0; i < numSteps; i++) {
+      it(`check that instruction ${i} is non-empty`, () => {
+        cy.get("directions-info")
+          .shadow()
+          .find(".text")
+          .then(($el) => {
+            expect($el).to.not.be.equal("");
+          });
+      });
+
+      it(`check that instruction ${i} is correctly parsed`, () => {
+        cy.get("directions-info")
+          .shadow()
+          .find(".text")
+          .then(($el) => {
+            expect($el[i].innerHTML).to.be.equal(
+              `${i + 1}) ` +
+                recipe.recipe1.analyzedInstructions[0].steps[i].step
+            );
+          });
+      });
+
+      it(`check that instruction ${i} has a checkbox`, () => {
+        cy.get("directions-info")
+          .shadow()
+          .find("input")
+          .then(($el) => {
+            expect($el[i].type).to.be.equal("checkbox");
+          });
+      });
+
+      it(`check that instruction ${i}'s checkbox is unchecked by default`, () => {
+        cy.get("directions-info")
+          .shadow()
+          .find("input")
+          .then(($el) => {
+            expect($el[i].checked).to.be.equal(false);
+          });
+      });
+    }
+    // check that there's a down-arrow src for each
+    // check that clicking the button does stuff (DEFINE STUFF)
   }
-  // check that the instructions have been correctly parsed out
-  // check that there's a checkbox for each
-  // check that there's a down-arrow src for each
-  // check that clicking the button does stuff (DEFINE STUFF)
-});
+);
