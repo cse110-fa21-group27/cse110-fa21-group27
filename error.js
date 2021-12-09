a2f9611879c46cf988dda5a60acdca81180ea45d
Tucker Frandsen <tfrandsen@ucsd.edu>
2021-12-09 13:27:47 -0800

    Removing unnecessary files

--- error.js
+++
@@ -1,9 +0,0 @@
-/** This is a JSDoc comment
- * @param {string} name This is good
- */
-function helloYou(name) {
-  name = "you" || name;
-  console.log("hello" + name + "!");
-}
-
-console.log(helloYou);
