d7352d74937958416d5c2230587f1ff4cf4aa79e
Miguel Christian Sanchez <mcsanche@ucsd.edu>
2021-12-09 20:20:53 -0800

    eslint red begone

--- source/js/SearchPage_2.js
+++ source/js/SearchPage_2.js
@@ -1,14 +1,18 @@
+/**
+ * This custom web component handles displaying search results
+ */
 class SearchPage extends HTMLElement {
-    /** constructs the component and allows access to the shadow */
-    constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: "open" });
-    }
-    /**
-     * @param {object} data - the recipe json file
-     */
-    set data(data) {
-      const style = `
+  /** constructs the component and allows access to the shadow */
+  constructor() {
+    super();
+    // eslint-disable-next-line no-unused-vars
+    const shadow = this.attachShadow({ mode: "open" });
+  }
+  /**
+   * @param {object} data - the recipe json file
+   */
+  set data(data) {
+    const style = `
         #filter {
           position: fixed;
           left: 0%;
@@ -104,236 +108,235 @@ class SearchPage extends HTMLElement {
           gap: 1em;
         }
       `;
-      const styleElem = document.createElement("style");
-      styleElem.innerHTML = style;
-  
-      const search_page = document.createElement("article");
-  
-      //##### start of filter #####
-      const filter = document.createElement("div");
-      filter.id = "filter";
-  
-      //filter label
-      const filterLabel = document.createElement("div");
-      filterLabel.id = "filter_label";
-      filterLabel.innerHTML = "Filter";
-      filter.appendChild(filterLabel);
-  
-      //clear all button
-      const clearAll = document.createElement("button");
-      clearAll.id = "clear_all";
-      clearAll.innerHTML = "CLEAR ALL";
-      filter.appendChild(clearAll);
-  
-      //hr
-      const hr = document.createElement("hr");
-      hr.style.marginTop = "25%";
-      hr.style.width = "90%";
-      hr.style.textAlign = "center";
-      hr.style.color = "black";
-      filter.appendChild(hr);
-  
-      //label Rating
-      const label_1 = document.createElement("div");
-      label_1.classList.add("label");
-      label_1.style.top = "80px";
-      label_1.innerHTML = "Rating";
-      filter.appendChild(label_1);
-  
-      //label Cooking Time
-      const label_2 = document.createElement("div");
-      label_2.classList.add("label");
-      label_2.style.top = "160px";
-      label_2.innerHTML = "Cooking Time";
-      filter.appendChild(label_2);
-  
-      //form 1
-      const form_1 = document.createElement("form");
-      form_1.classList.add("form");
-      form_1.style.top = "140px";
-  
-      const input_1 = document.createElement("input");
-      input_1.classList.add("input");
-      input_1.setAttribute("list", "input_list_1");
-      input_1.id = "input_1";
-      input_1.name = "input_1";
-  
-      const data_list_1 = document.createElement("datalist");
-      data_list_1.id = "input_list_1";
-      const option_1 = document.createElement("option");
-      option_1.value = "4 or more stars";
-      data_list_1.appendChild(option_1);
-      const option_2 = document.createElement("option");
-      option_2.value = "3 or more stars";
-      data_list_1.appendChild(option_2);
-      const option_3 = document.createElement("option");
-      option_3.value = "2 or more stars";
-      data_list_1.appendChild(option_3);
-      const option_4 = document.createElement("option");
-      option_4.value = "1 or more stars";
-      data_list_1.appendChild(option_4);
-  
-      input_1.appendChild(data_list_1);
-      form_1.appendChild(input_1);
-      filter.appendChild(form_1);
-  
-      //form 2
-      const form_2 = document.createElement("form");
-      form_2.classList.add("form");
-      form_2.style.top = "220px";
-  
-      const input_2 = document.createElement("input");
-      input_2.classList.add("input");
-      input_2.setAttribute("list", "input_list_2");
-      input_2.id = "input_2";
-      input_2.name = "input_2";
-  
-      const data_list_2 = document.createElement("datalist");
-      data_list_2.id = "input_list_2";
-      const option_5 = document.createElement("option");
-      option_5.value = "15 minutes or less";
-      data_list_2.appendChild(option_5);
-      const option_6 = document.createElement("option");
-      option_6.value = "30 minutes or less";
-      data_list_2.appendChild(option_6);
-      const option_7 = document.createElement("option");
-      option_7.value = "45 minutes or less";
-      data_list_2.appendChild(option_7);
-  
-      input_2.appendChild(data_list_2);
-      form_2.appendChild(input_2);
-      filter.appendChild(form_2);
-  
-      //label Vegetarian
-      const label_3 = document.createElement("div");
-      label_3.classList.add("label2");
-      label_3.style.top = "265px";
-      label_3.innerHTML = "Vegetarian";
-      filter.appendChild(label_3);
-  
-      //form 3
-      const form_3 = document.createElement("form");
-      form_3.classList.add("form");
-      form_3.style.top = "270px";
-  
-      const div_3 = document.createElement("div");
-      div_3.classList.add("box");
-  
-      const input_3 = document.createElement("input");
-      input_3.type = "checkbox";
-      input_3.id = "input_3";
-      input_3.name = "input_3";
-      input_3.value = "Vegetarian";
-      input_3.style.width = "40px";
-      input_3.style.height = "40px";
-  
-      div_3.appendChild(input_3);
-      form_3.appendChild(div_3);
-      filter.appendChild(form_3);
-  
-      //label Vegan
-      const label_4 = document.createElement("div");
-      label_4.classList.add("label2");
-      label_4.style.top = "345px";
-      label_4.innerHTML = "Vegan";
-      filter.appendChild(label_4);
-  
-      //form 4
-      const form_4 = document.createElement("form");
-      form_4.classList.add("form");
-      form_4.style.top = "350px";
-  
-      const div_4 = document.createElement("div");
-      div_4.classList.add("box");
-  
-      const input_4 = document.createElement("input");
-      input_4.type = "checkbox";
-      input_4.id = "input_4";
-      input_4.name = "input_4";
-      input_4.value = "Vegan";
-      input_4.style.width = "40px";
-      input_4.style.height = "40px";
-  
-      div_4.appendChild(input_4);
-      form_4.appendChild(div_4);
-      filter.appendChild(form_4);
-      
-      //label Gluten Free
-      const label_5 = document.createElement("div");
-      label_5.classList.add("label2");
-      label_5.style.top = "425px";
-      label_5.innerHTML = "Gluten Free";
-      filter.appendChild(label_5);
-  
-      //form 5
-      const form_5 = document.createElement("form");
-      form_5.classList.add("form");
-      form_5.style.top = "430px";
-  
-      const div_5 = document.createElement("div");
-      div_5.classList.add("box");
-  
-      const input_5 = document.createElement("input");
-      input_5.type = "checkbox";
-      input_5.id = "input_5";
-      input_5.name = "input_5";
-      input_5.value = "Gluten Free";
-      input_5.style.width = "40px";
-      input_5.style.height = "40px";
-  
-      div_5.appendChild(input_5);
-      form_5.appendChild(div_5);
-      filter.appendChild(form_5);
-  
-      //label Diary Free
-      const label_6 = document.createElement("div");
-      label_6.classList.add("label2");
-      label_6.style.top = "505px";
-      label_6.innerHTML = "Diary Free";
-      filter.appendChild(label_6);
-  
-      //form 5
-      const form_6 = document.createElement("form");
-      form_6.classList.add("form");
-      form_6.style.top = "510px";
-  
-      const div_6 = document.createElement("div");
-      div_6.classList.add("box");
-  
-      const input_6 = document.createElement("input");
-      input_6.type = "checkbox";
-      input_6.id = "input_6";
-      input_6.name = "input_6";
-      input_6.value = "Diary Free";
-      input_6.style.width = "40px";
-      input_6.style.height = "40px";
-  
-      div_6.appendChild(input_6);
-      form_6.appendChild(div_6);
-      filter.appendChild(form_6);
-  
-      //apply button
-      const apply = document.createElement("button");
-      apply.id = "apply";
-      apply.innerHTML = "APPLY";
-      filter.appendChild(apply);
-  
-      search_page.appendChild(filter);
-      //##### end of filter #####
-  
-      //##### start of search body ##### (#todo)
-      const body = document.createElement("div");
-      body.id = "search_body";
-      // create recipe cards for each result
-      this.renderRecipes(data, body);
-  
-      search_page.appendChild(body);
-      //##### end of search body #####
-  
-      this.shadowRoot.appendChild(styleElem);
-      this.shadowRoot.appendChild(search_page);
-    }
+    const styleElem = document.createElement("style");
+    styleElem.innerHTML = style;
+
+    const searchPage = document.createElement("article");
+
+    // ##### start of filter #####
+    const filter = document.createElement("div");
+    filter.id = "filter";
+
+    // filter label
+    const filterLabel = document.createElement("div");
+    filterLabel.id = "filter_label";
+    filterLabel.innerHTML = "Filter";
+    filter.appendChild(filterLabel);
+
+    // clear all button
+    const clearAll = document.createElement("button");
+    clearAll.id = "clear_all";
+    clearAll.innerHTML = "CLEAR ALL";
+    filter.appendChild(clearAll);
+
+    // hr
+    const hr = document.createElement("hr");
+    hr.style.marginTop = "25%";
+    hr.style.width = "90%";
+    hr.style.textAlign = "center";
+    hr.style.color = "black";
+    filter.appendChild(hr);
+
+    // label Rating
+    const label1 = document.createElement("div");
+    label1.classList.add("label");
+    label1.style.top = "80px";
+    label1.innerHTML = "Rating";
+    filter.appendChild(label1);
+
+    // label Cooking Time
+    const label2 = document.createElement("div");
+    label2.classList.add("label");
+    label2.style.top = "160px";
+    label2.innerHTML = "Cooking Time";
+    filter.appendChild(label2);
+
+    // form 1
+    const form1 = document.createElement("form");
+    form1.classList.add("form");
+    form1.style.top = "140px";
+
+    const input1 = document.createElement("input");
+    input1.classList.add("input");
+    input1.setAttribute("list", "input_list_1");
+    input1.id = "input1";
+    input1.name = "input1";
+
+    const dataList1 = document.createElement("datalist");
+    dataList1.id = "input_list_1";
+    const option1 = document.createElement("option");
+    option1.value = "4 or more stars";
+    dataList1.appendChild(option1);
+    const option2 = document.createElement("option");
+    option2.value = "3 or more stars";
+    dataList1.appendChild(option2);
+    const option3 = document.createElement("option");
+    option3.value = "2 or more stars";
+    dataList1.appendChild(option3);
+    const option4 = document.createElement("option");
+    option4.value = "1 or more stars";
+    dataList1.appendChild(option4);
+
+    input1.appendChild(dataList1);
+    form1.appendChild(input1);
+    filter.appendChild(form1);
+
+    // form 2
+    const form2 = document.createElement("form");
+    form2.classList.add("form");
+    form2.style.top = "220px";
+
+    const input2 = document.createElement("input");
+    input2.classList.add("input");
+    input2.setAttribute("list", "input_list_2");
+    input2.id = "input2";
+    input2.name = "input2";
+
+    const dataList2 = document.createElement("datalist");
+    dataList2.id = "input_list_2";
+    const option5 = document.createElement("option");
+    option5.value = "15 minutes or less";
+    dataList2.appendChild(option5);
+    const option6 = document.createElement("option");
+    option6.value = "30 minutes or less";
+    dataList2.appendChild(option6);
+    const option7 = document.createElement("option");
+    option7.value = "45 minutes or less";
+    dataList2.appendChild(option7);
+
+    input2.appendChild(dataList2);
+    form2.appendChild(input2);
+    filter.appendChild(form2);
+
+    // label Vegetarian
+    const label3 = document.createElement("div");
+    label3.classList.add("label2");
+    label3.style.top = "265px";
+    label3.innerHTML = "Vegetarian";
+    filter.appendChild(label3);
+
+    // form 3
+    const form3 = document.createElement("form");
+    form3.classList.add("form");
+    form3.style.top = "270px";
+
+    const div3 = document.createElement("div");
+    div3.classList.add("box");
+
+    const input3 = document.createElement("input");
+    input3.type = "checkbox";
+    input3.id = "input3";
+    input3.name = "input3";
+    input3.value = "Vegetarian";
+    input3.style.width = "40px";
+    input3.style.height = "40px";
+
+    div3.appendChild(input3);
+    form3.appendChild(div3);
+    filter.appendChild(form3);
+
+    // label Vegan
+    const label4 = document.createElement("div");
+    label4.classList.add("label2");
+    label4.style.top = "345px";
+    label4.innerHTML = "Vegan";
+    filter.appendChild(label4);
+
+    // form 4
+    const form4 = document.createElement("form");
+    form4.classList.add("form");
+    form4.style.top = "350px";
+
+    const div4 = document.createElement("div");
+    div4.classList.add("box");
+
+    const input4 = document.createElement("input");
+    input4.type = "checkbox";
+    input4.id = "input4";
+    input4.name = "input4";
+    input4.value = "Vegan";
+    input4.style.width = "40px";
+    input4.style.height = "40px";
+
+    div4.appendChild(input4);
+    form4.appendChild(div4);
+    filter.appendChild(form4);
+
+    // label Gluten Free
+    const label5 = document.createElement("div");
+    label5.classList.add("label2");
+    label5.style.top = "425px";
+    label5.innerHTML = "Gluten Free";
+    filter.appendChild(label5);
+
+    // form 5
+    const form5 = document.createElement("form");
+    form5.classList.add("form");
+    form5.style.top = "430px";
+
+    const div5 = document.createElement("div");
+    div5.classList.add("box");
+
+    const input5 = document.createElement("input");
+    input5.type = "checkbox";
+    input5.id = "input5";
+    input5.name = "input5";
+    input5.value = "Gluten Free";
+    input5.style.width = "40px";
+    input5.style.height = "40px";
+
+    div5.appendChild(input5);
+    form5.appendChild(div5);
+    filter.appendChild(form5);
+
+    // label Diary Free
+    const label6 = document.createElement("div");
+    label6.classList.add("label2");
+    label6.style.top = "505px";
+    label6.innerHTML = "Diary Free";
+    filter.appendChild(label6);
+
+    // form 5
+    const form6 = document.createElement("form");
+    form6.classList.add("form");
+    form6.style.top = "510px";
+
+    const div6 = document.createElement("div");
+    div6.classList.add("box");
+
+    const input6 = document.createElement("input");
+    input6.type = "checkbox";
+    input6.id = "input6";
+    input6.name = "input6";
+    input6.value = "Diary Free";
+    input6.style.width = "40px";
+    input6.style.height = "40px";
+
+    div6.appendChild(input6);
+    form6.appendChild(div6);
+    filter.appendChild(form6);
+
+    // apply button
+    const apply = document.createElement("button");
+    apply.id = "apply";
+    apply.innerHTML = "APPLY";
+    filter.appendChild(apply);
+
+    searchPage.appendChild(filter);
+    // ##### end of filter #####
+
+    // ##### start of search body ##### (#todo)
+    const body = document.createElement("div");
+    body.id = "search_body";
+    // create recipe cards for each result
+    this.renderRecipes(data, body);
+
+    searchPage.appendChild(body);
+    // ##### end of search body #####
+
+    this.shadowRoot.appendChild(styleElem);
+    this.shadowRoot.appendChild(searchPage);
   }
-  
-  customElements.define("search-page", SearchPage);
-  
+}
+
+customElements.define("search-page", SearchPage);
